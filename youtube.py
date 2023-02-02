44719ef7c72eaf7eb69666c88c546f6900de34df
KKaizer <matheus.kaizer@hotmail.com>
2023-02-02 15:36:50 -0300

    removendo o path

--- youtube.py
+++ youtube.py
@@ -5,12 +5,12 @@ def audio():
     yt = YouTube(sys.argv[1])
     audio
     audio_stream = yt.streams.filter(only_audio=True).first()
-    audio_stream.download("/home/kkaizer/Music/ytDL")
+    audio_stream.download()   #Adicione o path desejavel
 
 def video():
     yt = YouTube(sys.argv[1])
-    yt = yt.streams.get_highest_resolution()
-    yt.download("/home/kkaizer/Videos/ytVideo")
+    yt = yt.streams.get_highest_resolution() 
+    yt.download() #Adicione o path desejavel
 
 x=int(input("0 pra audio ou 1 para video: "))
 
