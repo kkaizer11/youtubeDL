7a2b28294e88432d05d4af1811979c675e723449
kkaizer11 <kaizerin.m@gmail.com>
2023-12-04 13:06:50 -0300

    ajustes a um código antigo

--- youtube.py
+++ youtube.py
@@ -1,24 +1,25 @@
 from pytube import YouTube
 import sys
 
+
 def audio():
     yt = YouTube(sys.argv[1])
-    audio
     audio_stream = yt.streams.filter(only_audio=True).first()
-    audio_stream.download()   #Adicione o path desejavel
+    audio_stream.download()  # Adicione o path desejavel
+
 
 def video():
     yt = YouTube(sys.argv[1])
-    yt = yt.streams.get_highest_resolution() 
-    yt.download() #Adicione o path desejavel
-
-x=int(input("0 pra audio ou 1 para video: "))
-
-if x == 0:
-    print("Audio")
-    audio()
-if x == 1:
-    print("Video")
-    video()
+    yt = yt.streams.get_highest_resolution()
+    yt.download()  # Adicione o path desejavel
 
 
+if __name__ == "__main__":
+    x = int(input("0 pra audio ou 1 para video: "))
+
+    if x == 0:
+        print("Audio")
+        audio()
+    if x == 1:
+        print("Video")
+        video()
